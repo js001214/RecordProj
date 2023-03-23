91b2ff89d18472341a3cbddfc4c3d78391b0f0ee
박주성 <juseong001214@gmail.com>
2023-03-23 20:14:32 +0900

    초기설정

---
+++ src/main/java/com/record/RecordProjApplication.java
@@ -0,0 +1,13 @@
+package com.record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/record/RecordProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
